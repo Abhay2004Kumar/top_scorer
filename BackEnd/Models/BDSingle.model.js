acb7e36677ae448a233ed1c5b48c75ab1889f9fb
piy3 <yadavpiy02@gmail.com>
2024-10-31 02:33:34 +0530

    user confirmation to submit match detail frontend done

--- BackEnd/Models/BDSingle.model.js
+++ BackEnd/Models/BDSingle.model.js
@@ -1,34 +1,5 @@
 import mongoose from "mongoose";
 
-// const PlayerSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   teamname: {
-//     type: String,
-//     required: true,
-//   },
-//   teamlogo: {
-//     type: String,
-//     required: true,
-//   },
-//   s1score: {
-//     type: Number,
-//     default: 0,
-//     min:0,
-//   },
-//   s2score: {
-//     type: Number,
-//     default: 0, 
-//     min:0,
-//   },
-//   s3score: {
-//     type: Number,
-//     default: 0,
-//     min:0,
-//   },
-// },{_id:false});
 
 const BDSingleSchema = new mongoose.Schema(
   {
