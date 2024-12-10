366be39e6622595d6ee0539fd2e2f01f4e06a0c6
Abhay2004Kumar <abs220502@gmail.com>
2024-12-11 02:56:05 +0530

    updated and fix commentBox

--- FrontEnd/topscorer/src/Components/Comment_Box/Comment_Box.jsx
+++ FrontEnd/topscorer/src/Components/Comment_Box/Comment_Box.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../Comment_Box/Comment_Box.module.css';
 import { RxAvatar } from "react-icons/rx";
 
-export default function Comment_Box({user,mssg}) {
+export default function Comment_Box({user,mssg,createdAt}) {
   return (
     <div className={styles.box}>
       <div className={styles.avatar}>
@@ -10,6 +10,7 @@ export default function Comment_Box({user,mssg}) {
       </div>
       <div className={styles.commentDetails}>
         <div className={styles.name}>{user}</div>
+       
         <div className={styles.commentText}>{mssg}</div>
       </div>
     </div>
